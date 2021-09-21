68a794f6031c6a7e4dd46a6e51d6f46980b8c6be
PaytonCrawford <W0742398@myscc.ca>
2021-09-21 13:55:47 -0400

    Saved import changes

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContentCardComponent} from "./content-card/content-card.component";
+import {Content} from "@angular/compiler/src/render3/r3_ast";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'learning-angular';
 
   public ngOnInit() {
+    contentList: [] = [{
 
+    }]
   }
 
 }
--- learning-angular/src/app/content-card/content-card.component.ts
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -1,19 +1,17 @@
 // @ts-ignore
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 // @ts-ignore
 import { ContentList } from 'helper-files/content-interface';
 
 
 
-// @ts-ignore
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-
-  content: ContentList;
+  @Input() content: any;
   constructor() {
 
   }
