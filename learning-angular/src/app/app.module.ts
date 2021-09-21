dd32992209341107448dee7c3ccfcc6fae03f6de
PaytonCrawford <W0742398@myscc.ca>
2021-09-20 21:19:51 -0400

    Assignment 1 Commit

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { PipePipe } from './pipe.pipe';
+import { DirectiveDirective } from './directive.directive';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    PipePipe,
+    DirectiveDirective,
+    ContentCardComponent,
+    TestcomponentComponent
   ],
   imports: [
     BrowserModule
---
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import { ContentList } from 'helper-files/content-interface';
+
+
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    let contentItem: ContentList = {
+      id: 1,
+      author: "Payton",
+      title: "Test Title"
+    }
+
+
+  }
+}
