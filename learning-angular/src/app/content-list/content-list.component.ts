cf22a4193538054d56306665f4b66b7118793654
PaytonCrawford <W0742398@myscc.ca>
2021-09-27 20:21:38 -0400

    Fixed HTML template

--- learning-angular/src/app/content-list/content-list.component.ts
+++ learning-angular/src/app/content-list/content-list.component.ts
@@ -12,31 +12,31 @@ export class ContentListComponent implements OnInit {
   constructor() {
     this.contentArray = [{
       id: 1,
-      author: "Payton",
+      author: "Payton Crawford",
       title: "Basketball",
       imgUrl: "https://cdn.pixabay.com/photo/2015/05/15/14/49/basketball-768713__340.jpg",
       body: "Michael Jordan is the best basketball player of all time."
     }, {
       id: 2,
-      author: "Payton",
+      author: "Payton Crawford",
       title: "Football",
       imgUrl: "https://cdn.pixabay.com/photo/2016/11/22/23/30/american-football-1851168_960_720.jpg",
       body: "Football is an american sport."
     }, {
       id: 3,
-      author: "Payton",
+      author: "Payton Crawford",
       title: "Hockey",
       imgUrl: "https://cdn.pixabay.com/photo/2017/03/28/02/17/hockey-2180834_960_720.jpg",
       body: "Hockey is played on ice."
     }, {
       id: 4,
-      author: "Payton",
+      author: "Payton Crawford",
       title: "Baseball",
       imgUrl: "https://cdn.pixabay.com/photo/2016/05/22/01/38/hit-1407826_960_720.jpg",
       body: "Pretty boring..."
     }, {
       id: 5,
-      author: "Payton",
+      author: "Payton Crawford",
       title: "Golf",
       imgUrl: "https://cdn.pixabay.com/photo/2013/02/09/13/39/tiger-woods-79694_960_720.jpg",
       body: "The best sport in the world."
