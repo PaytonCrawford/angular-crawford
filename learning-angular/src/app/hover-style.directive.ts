b3cba7c3199b45f8ea849d0b29f7f9c2b8790077
PaytonCrawford <W0742398@myscc.ca>
2021-10-08 16:25:27 -0400

    bold tag elements when hovered over

--- learning-angular/src/app/hover-style.directive.ts
+++ learning-angular/src/app/hover-style.directive.ts
@@ -11,14 +11,21 @@ export class HoverStyleDirective{
 
   @HostListener('mouseenter') onMouseEnter() {
     this.underline('underline')
+    this.bold('bold')
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.underline('none')
+    this.bold('normal')
   }
 
   underline(underline: string): void {
     this.element.nativeElement.style.textDecoration = underline || 'underline';
   }
 
+  bold(bold: string): void {
+    this.element.nativeElement.style.fontWeight = bold || 'bold';
+  }
+
 }
+
