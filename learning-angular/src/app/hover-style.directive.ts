4423f7795e086acd52978a60e1e5598b4ecef85a
PaytonCrawford <W0742398@myscc.ca>
2021-10-08 14:11:51 -0400

    added hover listener

--- learning-angular/src/app/hover-style.directive.ts
+++ learning-angular/src/app/hover-style.directive.ts
@@ -1,12 +1,23 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHoverStyle]'
 })
 export class HoverStyleDirective {
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
   constructor(private element: ElementRef) {
     element.nativeElement.style.underline
   }
 
+  private highlight(colour: string) {
+    this.element.nativeElement.style.backgroundColor = colour
+  }
 }
