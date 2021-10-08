7630fae1f39b79ab596afda5dd8d4161c10e3d19
PaytonCrawford <W0742398@myscc.ca>
2021-10-08 14:14:57 -0400

    underline changed

--- learning-angular/src/app/hover-style.directive.ts
+++ learning-angular/src/app/hover-style.directive.ts
@@ -6,18 +6,19 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
 export class HoverStyleDirective {
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('red');
+    this.underline();
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight('');
+
   }
 
   constructor(private element: ElementRef) {
     element.nativeElement.style.underline
   }
 
-  private highlight(colour: string) {
-    this.element.nativeElement.style.backgroundColor = colour
+  private underline() {
+    this.element.nativeElement.style.underline
   }
+
 }
