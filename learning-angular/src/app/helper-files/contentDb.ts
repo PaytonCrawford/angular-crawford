50a4055754d3354f5222937d894927648d6a8bea
PaytonCrawford <W0742398@myscc.ca>
2021-11-08 21:44:32 -0500

    re-added tags

--- learning-angular/src/app/helper-files/contentDb.ts
+++ learning-angular/src/app/helper-files/contentDb.ts
@@ -6,39 +6,45 @@ export const CONTENT: Content[] = [{
     title: "Basketball",
     type: "Team Sport",
     imgUrl: "https://cdn.pixabay.com/photo/2015/05/15/14/49/basketball-768713__340.jpg",
-    body: "Michael Jordan is the best basketball player of all time."
+    body: "Michael Jordan is the best basketball player of all time.",
+    tags: ["Basketball", "Jordan"]
   }, {
     id: 2,
     author: "Payton Crawford",
     title: "Football",
     type: "Team Sport",
     imgUrl: "https://cdn.pixabay.com/photo/2016/11/22/23/30/american-football-1851168_960_720.jpg",
-    body: "Football is an american sport."
+    body: "Football is an american sport.",
+    tags: ["Lombardi", "Field"]
   }, {
     id: 3,
     author: "Payton Crawford",
     title: "Hockey",
     type: "Team Sport",
     imgUrl: "https://cdn.pixabay.com/photo/2017/03/28/02/17/hockey-2180834_960_720.jpg",
-    body: "Hockey is played on ice."
+    body: "Hockey is played on ice.",
+    tags: ["Puck", "Ice"]
   }, {
     id: 4,
     author: "Payton Crawford",
     title: "Baseball",
     type: "Team Sport",
     imgUrl: "https://cdn.pixabay.com/photo/2016/05/22/01/38/hit-1407826_960_720.jpg",
-    body: "Pretty boring..."
+    body: "Pretty boring...",
+    tags: ["Boring", "Bat"]
   }, {
     id: 5,
     author: "Payton Crawford",
     title: "Esports",
     type: "Team Sport",
-    body: "Competitive variety of video games."
+    body: "Competitive variety of video games.",
+    tags: ["Gaming", "CSGO"]
   }, {
     id: 6,
     author: "Payton Crawford",
     title: "Golf",
     type: "Individual Sport",
     imgUrl: "https://cdn.pixabay.com/photo/2013/02/09/13/39/tiger-woods-79694_960_720.jpg",
-    body: "The best sport in the world."
+    body: "The best sport in the world.",
+    tags: ["Tiger", "Putting"]
   }];
